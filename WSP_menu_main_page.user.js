310273a48668304eaaf0006c61de5a7180040ed4
Pavel <79376580+pertsevpy@users.noreply.github.com>
2025-02-11 19:12:09 +0300

    Fixed the close button. Other minor fixes.

--- WSP_menu_main_page.user.js
+++ WSP_menu_main_page.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WSP_menu_main_page
 // @namespace    https://github.com/pertsevpy/WSP/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Additional functions for the main menu
 // @author       Pavel P.
 // @license      Unlicense
@@ -36,6 +36,9 @@
    For more information, please refer to <http://unlicense.org/>
 */
 
+const USERNAME = '1'; // the name of the user for which to check the pinning of objects
+// TODO make user selection (drop down list)
+
 (function () {
     'use strict';
 
@@ -46,61 +49,63 @@
     fetchButton.textContent = 'Список объектов';
     buttonContainer.insertBefore(fetchButton, buttonContainer.firstChild);
 
-fetchButton.addEventListener('click', () => {
-    const SERVER_URL = window.location.host;
-    // Getting data from the first API
-    fetchData('http://'+SERVER_URL+'/api/managerobject/')
-        .then(objectData => {
-            const tableData = [];
-            let promises = [];
+    // Button click handler
+    fetchButton.addEventListener('click', () => {
+        const SERVER_URL = window.location.host;
 
-            // Creating a table
+        // Getting data from API
+        fetchData(`http://${SERVER_URL}/api/managerobject/`)
+            .then(objectData => {
+            const tableData = [];
+            const promises = [];
+
+            //Getting user data
+            const userObjectSetPromise = fetchData(`http://${SERVER_URL}/api/objectsuser/${USERNAME}`)
+            .then(generalData => new Set(generalData));
+
+            // Create a table
             objectData.forEach(obj => {
                 if (obj.numobj) {
-                    // Creating a promise for fetching owner data
-                    const ownerPromise = fetchData(`http://`+SERVER_URL+`/api/owner/${obj.numobj}`).then(ownerData => {
-                        return {
-                            numobj: obj.numobj,
-                            nameobj: obj.nameobj,
-                            fio: ownerData.fio || '',
-                            mobile_phone: ownerData.mobile_phone || '',
-                            mobile_phone2: ownerData.mobile_phone2 || ''
-                        };
-                    });
+                    // We receive the owner's data and general data
+                    const ownerPromise = fetchData(`http://${SERVER_URL}/api/owner/${obj.numobj}`)
+                    .then(ownerData => ({
+                        numobj: obj.numobj,
+                        nameobj: obj.nameobj,
+                        fio: ownerData.fio || '',
+                        mobile_phone: ownerData.mobile_phone || '',
+                        mobile_phone2: ownerData.mobile_phone2 || ''
+                    }));
 
-                    // Creating a promise for fetching general data
-                    const generalPromise = fetchData(`http://`+SERVER_URL+`/api/general/${obj.numobj}`).then(generalData => {
-                        return {
-                            town_name: generalData.town_name || '',
-                            region_name: generalData.region_name || '',
-                            street_name: generalData.street_name || '',
-                            number_building: generalData.number_building || ''
-                        };
-                    });
+                    const generalPromise = fetchData(`http://${SERVER_URL}/api/general/${obj.numobj}`)
+                    .then(generalData => ({
+                        town_name: generalData.town_name || '',
+                        region_name: generalData.region_name || '',
+                        street_name: generalData.street_name || '',
+                        number_building: generalData.number_building || ''
+                    }));
 
-                    // Combining both promises to create the final object
+                    // Combining data
                     promises.push(Promise.all([ownerPromise, generalPromise]).then(([ownerData, generalData]) => {
-                        // Create the full entry with address
-                        const fullData = {
+                        return userObjectSetPromise.then(userObjectSet => ({
                             ...ownerData,
-                            address: `${generalData.town_name}, ${generalData.region_name}, ${generalData.street_name}, ${generalData.number_building}`
-                        };
-                        tableData.push(fullData);
+                            address: `${generalData.town_name}, ${generalData.region_name}, ${generalData.street_name}, ${generalData.number_building}`,
+                            user: userObjectSet.has(obj.numobj.toString()) ? '1' : ''
+                        }));
                     }));
                 }
             });
 
-            // We are waiting for all promises to complete
-            return Promise.all(promises).then(() => {
-                // Sort by numobj
+            // Waiting for all requests to complete
+            return Promise.all(promises).then(results => {
+                tableData.push(...results);
                 tableData.sort((a, b) => a.numobj - b.numobj);
                 showModal(tableData);
             });
         })
-        .catch(error => {
+            .catch(error => {
             console.error('Error while receiving data:', error);
         });
-});
+    });
 
     // Function to execute the request
     function fetchData(url) {
@@ -111,8 +116,7 @@ fetchButton.addEventListener('click', () => {
                 onload: (response) => {
                     if (response.status >= 200 && response.status < 300) {
                         try {
-                            const jsonData = JSON.parse(response.responseText);
-                            resolve(jsonData);
+                            resolve(JSON.parse(response.responseText));
                         } catch (e) {
                             reject('Error parsing JSON: ' + e);
                         }
@@ -127,10 +131,29 @@ fetchButton.addEventListener('click', () => {
         });
     }
 
+    // Function to create a close button
+    function createCloseButton(modal) {
+        const closeButton = document.createElement('button');
+        closeButton.textContent = 'Закрыть';
+        closeButton.style.float = 'right';
+        closeButton.classList.add('close');
+        closeButton.setAttribute('data-dismiss', 'modal');
+        closeButton.setAttribute('aria-hidden', 'true');
+
+        closeButton.addEventListener('click', () => {
+            modal.setAttribute('aria-hidden', 'true');
+            modal.classList.remove('in');
+            modal.style.display = 'none';
+            document.body.removeChild(modal);
+        });
+
+        return closeButton;
+    }
+
     // Function to create and display a modal window
     function showModal(data) {
         const modal = document.createElement('div');
-        modal.className = 'modal fade in'; // WPS
+        modal.className = 'modal fade in';
         modal.style.display = 'block';
         modal.style.position = 'fixed';
         modal.style.top = '0';
@@ -149,18 +172,7 @@ fetchButton.addEventListener('click', () => {
         modalContent.style.maxHeight = '80%';
         modalContent.style.overflowY = 'auto';
 
-        const closeButton = document.createElement('button');
-        closeButton.textContent = 'Закрыть';
-        closeButton.style.float = 'right';
 
-        closeButton.addEventListener('click', () => {
-            console.log('Close modal');
-            modal.classList.remove('in'); 
-            modal.style.display = 'none';
-            document.body.removeChild(modal);
-        });
-
-        modalContent.appendChild(closeButton);
         modalContent.innerHTML += `<h5>Данные объектов</h5>`;
         modalContent.innerHTML += `<table class='table'><thead>
             <tr>
@@ -170,6 +182,7 @@ fetchButton.addEventListener('click', () => {
                 <th>Телефон 1</th>
                 <th>Телефон 2</th>
                 <th>Адрес</th>
+                <th>Закреплен</th>
             </tr>
         </thead><tbody>
             ${data.map(item => `
@@ -180,11 +193,15 @@ fetchButton.addEventListener('click', () => {
                     <td>${item.mobile_phone}</td>
                     <td>${item.mobile_phone2}</td>
                     <td>${item.address}</td>
+                    <td>${item.user}</td>
                 </tr>`).join('')}
         </tbody></table>`;
+
+        const closeButton = createCloseButton(modal);
+        modalContent.insertBefore(closeButton, modalContent.firstChild);
+        // modalContent.appendChild(closeButton);
 
         modal.appendChild(modalContent);
         document.body.appendChild(modal);
     }
-
 })();
